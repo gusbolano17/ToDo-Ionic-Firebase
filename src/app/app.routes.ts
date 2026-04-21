9eb5253c260886e64431989c5835746f563e6d5c
Gustavo Adolfo Bolaño Ramos <gustavoadolfobolano@gmail.com>
2026-04-20 19:21:58 -0500

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    loadComponent: () => import('./tareas/tareas.page').then(m => m.TareasPage)
   },
 ];
